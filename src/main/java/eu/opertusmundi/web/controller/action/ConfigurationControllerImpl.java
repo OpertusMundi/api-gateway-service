d2995644bb5df0563f59b19a5c547f86509fb132
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2020-12-09 20:11:21 +0200

    [api-gateway][cli] Add asset file types to configuration

--- src/main/java/eu/opertusmundi/web/controller/action/ConfigurationControllerImpl.java
+++ src/main/java/eu/opertusmundi/web/controller/action/ConfigurationControllerImpl.java
@@ -2,12 +2,15 @@ package eu.opertusmundi.web.controller.action;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import eu.opertusmundi.common.domain.AssetFileTypeEntity;
 import eu.opertusmundi.common.model.EnumAuthProvider;
 import eu.opertusmundi.common.model.RestResponse;
+import eu.opertusmundi.common.repository.AssetFileTypeRepository;
 import eu.opertusmundi.web.model.configuration.ClientConfiguration;
 
 @RestController
@@ -16,6 +19,9 @@ public class ConfigurationControllerImpl implements ConfigurationController {
 
     @Value("${opertus-mundi.authentication-providers:forms}")
     private String authProviders;
+
+    @Autowired
+    private AssetFileTypeRepository assetFileTypeRepository;
 
     @Override
     public RestResponse<ClientConfiguration> getConfiguration(String locale) {
@@ -30,6 +36,10 @@ public class ConfigurationControllerImpl implements ConfigurationController {
             .map(EnumAuthProvider::fromString)
             .filter(s -> s != null)
             .forEach(s -> config.getAuthProviders().add(s));
+
+        this.assetFileTypeRepository.findAllEnabled().parallelStream()
+            .map(AssetFileTypeEntity::toDto)
+            .forEach(t -> config.getAsset().getFileTypes().add(t));
 
         return config;
     }
--- src/main/java/eu/opertusmundi/web/model/configuration/ClientConfiguration.java
+++ src/main/java/eu/opertusmundi/web/model/configuration/ClientConfiguration.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import eu.opertusmundi.common.model.EnumAuthProvider;
+import eu.opertusmundi.common.model.asset.AssetFileTypeDto;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
 
 /**
  * Application configuration settings
@@ -19,10 +21,25 @@ public class ClientConfiguration {
         minItems = 0,
         uniqueItems = true
     )
+    @Getter
 	private final List<EnumAuthProvider> authProviders = new ArrayList<EnumAuthProvider>();
 
-	public List<EnumAuthProvider> getAuthProviders() {
-		return this.authProviders;
-	}
+    @Schema(description = "Asset related configuration settings")
+    @Getter
+    private final AssetConfiguration asset = new AssetConfiguration();
+
+    public static class AssetConfiguration {
+
+        @ArraySchema(
+            arraySchema = @Schema(
+                description = "Supported asset file types"
+            ),
+            minItems = 0,
+            uniqueItems = true
+        )
+        @Getter
+        private final List<AssetFileTypeDto> fileTypes = new ArrayList<AssetFileTypeDto>();
+
+    }
 
 }
