6486dd1ac27c239c57399b79d87c874a92025aec
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2021-03-02 09:29:05 +0200

    Create separate configuration file for Feign clients

--- api-gateway/src/main/java/eu/opertusmundi/web/config/FeignClientConfiguration.java
+++ api-gateway/src/main/java/eu/opertusmundi/web/config/FeignClientConfiguration.java
@@ -2,6 +2,7 @@ package eu.opertusmundi.web.config;
 
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @EnableFeignClients(
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
         eu.opertusmundi.web.feign.client._Marker.class,
     }
 )
+@PropertySource("classpath:config/feign-client.properties")
 public class FeignClientConfiguration {
 
     // Add any custom bean definitions here
