8fca908400e2c3bfa35561c8553224693793afd3
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2021-03-24 15:17:17 +0200

    Add controller for MANGOPAY webhooks

---
+++ api-gateway/src/main/java/eu/opertusmundi/web/controller/webhook/MangoPayWebhookController.java
@@ -0,0 +1,99 @@
+package eu.opertusmundi.web.controller.webhook;
+
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import io.swagger.v3.oas.annotations.Hidden;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+
+@Hidden
+@RequestMapping(path = "/webhooks")
+public interface MangoPayWebhookController {
+
+    /**
+     * Handles MANGOPAY webhook events
+     *
+     * For details on method performance requirements see MANGOPAY documentation.
+     * 
+     * @see <a href="https://docs.mangopay.com/endpoints/v2.01/hooks#e246_the-hook-object">The Hook object</a>
+     * 
+     * @param resourceId
+     * @param timestamp
+     * @param eventType
+     * @return
+     */
+    @Operation(
+        operationId = "webhook-01",
+        summary     = "MANGOPAY",
+        description = "Handles MANGOPAY webhook events"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "successful operation"
+    )
+    @GetMapping(value = "/mangopay")
+    ResponseEntity<Void> mangoPayWebhookHandler(
+        @Parameter(
+            in = ParameterIn.QUERY,
+            required = true,
+            description = "The ID of whatever the event is"
+        )
+        @RequestParam(name = "RessourceId", required = true) String resourceId,
+        @Parameter(
+            in = ParameterIn.QUERY,
+            required = true,
+            description = "When the event happened"
+        )
+        @RequestParam(name = "Date", required = true) Long timestamp,
+        @Parameter(
+            in = ParameterIn.QUERY,
+            required = true,
+            description = "The event type"
+        )
+        @RequestParam(name = "EventType", required = true) String eventType
+    );
+
+    /**
+     * Handles 3-D Secure validation redirects
+     *
+     * @see <a href="https://docs.mangopay.com/endpoints/v2.01/payins#e278_create-a-card-direct-payin">Create a Card Direct PayIn</a>
+     * 
+     * @param resourceId
+     * @param timestamp
+     * @param eventType
+     * @return
+     */
+    @Operation(
+        operationId = "webhook-02",
+        summary     = "3D Secure",
+        description = "3D secure validation redirect URL"
+    )
+    @ApiResponse(
+        responseCode = "200",
+        description = "successful operation"
+    )
+    @GetMapping(value = "/payins/{payInKey}")
+    String webhookHandler(
+        @Parameter(
+            in = ParameterIn.PATH,
+            required = true,
+            description = "The PayIn platform unique identifier"
+        )
+        @PathVariable(name = "payInKey") UUID payInKey,
+        @Parameter(
+            in = ParameterIn.QUERY,
+            required = true,
+            description = "The payment provider transaction identifier (PayIn identifier)"
+        )
+        @RequestParam(name = "transactionId", required = true) String transactionId
+    );
+
+}
---
+++ api-gateway/src/main/java/eu/opertusmundi/web/controller/webhook/MangoPayWebhookControllerImpl.java
@@ -0,0 +1,44 @@
+package eu.opertusmundi.web.controller.webhook;
+
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+import eu.opertusmundi.common.service.PaymentService;
+
+@Controller
+public class MangoPayWebhookControllerImpl implements MangoPayWebhookController {
+
+    private static final Logger logger = LoggerFactory.getLogger("WEBHOOK");
+    
+    @Autowired
+    PaymentService paymentService;
+
+    @Override
+    public ResponseEntity<Void> mangoPayWebhookHandler(String resourceId, Long timestamp, String eventType) {
+        logger.info(String.format("%30s %30s %30s", eventType, resourceId, timestamp));
+        
+        final ZonedDateTime date = ZonedDateTime.ofInstant(Instant.ofEpochSecond(timestamp), ZoneOffset.UTC);
+        
+        this.paymentService.handleWebHook(eventType, resourceId, date);
+        
+        // Webhook received successfully
+        return new ResponseEntity<Void>(HttpStatus.OK);
+    }
+
+    @Override
+    public String webhookHandler(UUID payInKey, String transactionId) {
+        this.paymentService.updatePayIn(payInKey, transactionId);
+        
+        return "index";
+    }
+            
+}
