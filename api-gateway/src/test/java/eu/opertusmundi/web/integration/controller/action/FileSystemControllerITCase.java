936b110edf221d28c02250d9a380264cc56b9eb3
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2021-04-22 17:09:42 +0300

    Remove proxy example

--- api-gateway/src/test/java/eu/opertusmundi/web/integration/controller/action/FileSystemControllerITCase.java
+++ api-gateway/src/test/java/eu/opertusmundi/web/integration/controller/action/FileSystemControllerITCase.java
@@ -22,7 +22,7 @@ import java.util.Set;
 import java.util.UUID;
 
 import org.apache.commons.io.FileUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
