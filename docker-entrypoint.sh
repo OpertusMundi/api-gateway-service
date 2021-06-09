fc65c4663d9ada8de5f8f01514770e14ec12bb44
Michail Alexakis <alexakis@imis.athena-innovation.gr>
2021-06-09 11:57:44 +0300

    Remove trailing slash for Elasticsearch base URLs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -78,7 +78,7 @@ runtime_profile=$(hostname | md5sum | head -c10)
     pid_base_url=$(echo ${PID_BASE_URL} | _validate_http_url "PID_BASE_URL")
     echo "opertusmundi.feign.persistent-identifier-service.url= ${pid_base_url}"
 
-    elasticsearch_base_url=$(echo ${ELASTICSEARCH_BASE_URL} | _validate_http_url "ELASTICSEARCH_BASE_URL")
+    elasticsearch_base_url=$(echo ${ELASTICSEARCH_BASE_URL%/} | _validate_http_url "ELASTICSEARCH_BASE_URL")
     elasticsearch_indices_assets_index_name=${ELASTICSEARCH_INDICES_ASSETS_INDEX_NAME}
     elasticsearch_indices_assets_view_index_name=${ELASTICSEARCH_INDICES_ASSETS_VIEW_INDEX_NAME}
     elasticsearch_indices_assets_view_aggregate_index_name=${ELASTICSEARCH_INDICES_ASSETS_VIEW_AGGREGATE_INDEX_NAME}
