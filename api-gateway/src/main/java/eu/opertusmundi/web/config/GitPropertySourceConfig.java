b2f93f7c7bbaeadfbddca35f148b879406e338e5
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2021-08-25 12:08:19 +0300

    Ignore Git property source configuration for tests

--- api-gateway/src/main/java/eu/opertusmundi/web/config/GitPropertySourceConfig.java
+++ api-gateway/src/main/java/eu/opertusmundi/web/config/GitPropertySourceConfig.java
@@ -1,8 +1,10 @@
 package eu.opertusmundi.web.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
+@Profile({"!testing"})
 @Configuration
 @PropertySource("classpath:git.properties")
 public class GitPropertySourceConfig {
