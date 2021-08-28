a78535058a91d78f2247f61c9003ebcf604da16b
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2021-08-28 21:06:44 +0300

    Update consumer contract controller
    Check if an order exists and belongs to the authenticated user before printing a contract.
    Allow to select orders with status `CREATED`.

--- api-gateway/src/main/java/eu/opertusmundi/web/controller/action/ConsumerContractControllerImpl.java
+++ api-gateway/src/main/java/eu/opertusmundi/web/controller/action/ConsumerContractControllerImpl.java
@@ -14,12 +14,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
+import eu.opertusmundi.common.domain.OrderEntity;
 import eu.opertusmundi.common.model.contract.consumer.PrintConsumerContractCommand;
 import eu.opertusmundi.common.model.contract.consumer.SignConsumerContractCommand;
+import eu.opertusmundi.common.repository.OrderRepository;
 import eu.opertusmundi.common.service.contract.ConsumerContractService;
 import eu.opertusmundi.common.service.contract.ContractFileManager;
 
@@ -30,12 +33,17 @@ public class ConsumerContractControllerImpl extends BaseController implements Co
     private ContractFileManager fileManager;
 
     @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
     private ConsumerContractService contractService;
 
     @Override
     public ResponseEntity<StreamingResponseBody> print(
         UUID orderKey, Integer itemIndex, HttpServletResponse response
     ) {
+        this.ensureOwner(orderKey);
+
         // Path will be resolved by the contract service
         final PrintConsumerContractCommand command = PrintConsumerContractCommand.builder()
             .userId(this.currentUserId())
@@ -54,6 +62,8 @@ public class ConsumerContractControllerImpl extends BaseController implements Co
     public ResponseEntity<StreamingResponseBody> sign(
         UUID orderKey, Integer itemIndex, HttpServletResponse response
     ) throws IOException {
+        this.ensureOwner(orderKey);
+
         // Paths will be resolved by the contract service
         final SignConsumerContractCommand command = SignConsumerContractCommand.builder()
             .userId(this.currentUserId())
@@ -72,6 +82,8 @@ public class ConsumerContractControllerImpl extends BaseController implements Co
     public ResponseEntity<StreamingResponseBody> download(
         UUID orderKey, Integer itemIndex, boolean signed, HttpServletResponse response
     ) {
+        this.ensureOwner(orderKey);
+
         final Path path = this.fileManager.resolvePath(this.currentUserId(), orderKey, itemIndex, signed, true);
         final File file = path.toFile();
 
@@ -94,6 +106,14 @@ public class ConsumerContractControllerImpl extends BaseController implements Co
         };
 
         return new ResponseEntity<StreamingResponseBody>(stream, HttpStatus.OK);
+    }
+
+    private void ensureOwner(UUID orderKey) {
+        final OrderEntity order = this.orderRepository.findEntityByKeyAndConsumerKey(this.currentUserKey(), orderKey).orElse(null);
+
+        if (order == null) {
+            throw new AccessDeniedException("Access denied");
+        }
     }
 
 }
