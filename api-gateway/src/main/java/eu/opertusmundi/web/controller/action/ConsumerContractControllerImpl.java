9670b0eccd9abae72d20e398549253e252c6b20c
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2021-11-11 14:40:09 +0200

    Set contract download file name
    Set contact download file name to order reference number.

--- api-gateway/src/main/java/eu/opertusmundi/web/controller/action/ConsumerContractControllerImpl.java
+++ api-gateway/src/main/java/eu/opertusmundi/web/controller/action/ConsumerContractControllerImpl.java
@@ -19,10 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
-import eu.opertusmundi.common.domain.OrderEntity;
 import eu.opertusmundi.common.model.contract.EnumContract;
 import eu.opertusmundi.common.model.contract.consumer.PrintConsumerContractCommand;
 import eu.opertusmundi.common.model.contract.consumer.SignConsumerContractCommand;
+import eu.opertusmundi.common.model.order.OrderDto;
 import eu.opertusmundi.common.repository.OrderRepository;
 import eu.opertusmundi.common.service.contract.ConsumerContractService;
 import eu.opertusmundi.common.service.contract.ContractFileManager;
@@ -43,7 +43,7 @@ public class ConsumerContractControllerImpl extends BaseController implements Co
     public ResponseEntity<StreamingResponseBody> print(
         UUID orderKey, Integer itemIndex, HttpServletResponse response
     ) {
-        this.ensureOwner(orderKey);
+        final OrderDto order = this.ensureOwner(orderKey);
 
         // Path will be resolved by the contract service
         final PrintConsumerContractCommand command = PrintConsumerContractCommand.builder()
@@ -57,14 +57,14 @@ public class ConsumerContractControllerImpl extends BaseController implements Co
 
         final File contractFile = command.getPath().toFile();
 
-        return this.createResponse(response, contractFile);
+        return this.createResponse(response, contractFile, this.getFilename(order));
     }
 
     @Override
     public ResponseEntity<StreamingResponseBody> sign(
         UUID orderKey, Integer itemIndex, HttpServletResponse response
     ) throws IOException {
-        this.ensureOwner(orderKey);
+        final OrderDto order = this.ensureOwner(orderKey);
 
         // Paths will be resolved by the contract service
         final SignConsumerContractCommand command = SignConsumerContractCommand.builder()
@@ -77,14 +77,14 @@ public class ConsumerContractControllerImpl extends BaseController implements Co
 
         final File contractFile = command.getTargetPath().toFile();
 
-        return this.createResponse(response, contractFile);
+        return this.createResponse(response, contractFile, this.getFilename(order));
     }
 
     @Override
     public ResponseEntity<StreamingResponseBody> download(
         UUID orderKey, Integer itemIndex, boolean signed, HttpServletResponse response
     ) {
-        this.ensureOwner(orderKey);
+        final OrderDto order = this.ensureOwner(orderKey);
 
         final Path path = this.fileManager.resolvePath(this.currentUserId(), orderKey, itemIndex, signed, true);
         final File file = path.toFile();
@@ -93,11 +93,13 @@ public class ConsumerContractControllerImpl extends BaseController implements Co
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Contract not found");
         }
 
-        return this.createResponse(response, file);
+        return this.createResponse(response, file, this.getFilename(order));
     }
 
-    private ResponseEntity<StreamingResponseBody> createResponse(HttpServletResponse response, File file) {
-        response.setHeader("Content-Disposition", String.format("attachment; filename=%s", file.getName()));
+    private ResponseEntity<StreamingResponseBody> createResponse(
+        HttpServletResponse response, File file, String downloadFilename
+    ) {
+        response.setHeader("Content-Disposition", String.format("attachment; filename=%s", downloadFilename));
         response.setHeader("Content-Type", MediaType.APPLICATION_PDF_VALUE);
         response.setHeader("Content-Length", Long.toString(file.length()));
 
@@ -110,12 +112,18 @@ public class ConsumerContractControllerImpl extends BaseController implements Co
         return new ResponseEntity<StreamingResponseBody>(stream, HttpStatus.OK);
     }
 
-    private void ensureOwner(UUID orderKey) {
-        final OrderEntity order = this.orderRepository.findEntityByKeyAndConsumerKey(this.currentUserKey(), orderKey).orElse(null);
+    private OrderDto ensureOwner(UUID orderKey) {
+        final OrderDto order = this.orderRepository.findObjectByKeyAndConsumerKey(this.currentUserKey(), orderKey).orElse(null);
 
         if (order == null) {
             throw new AccessDeniedException("Access denied");
         }
+
+        return order;
+    }
+
+    private String getFilename(OrderDto order) {
+        return order.getReferenceNumber() + ".pdf";
     }
 
 }
