e76c296ad714bf1fc573b00e7a6ff1fede22f750
Yannis Kouvaras <yannis.kouvaras@kupa.gr>
2022-06-24 08:24:10 +0300

    Fix Spring MVC SimpleAsyncTaskExecutor warning

--- api-gateway/src/main/java/eu/opertusmundi/web/config/MvcConfiguration.java
+++ api-gateway/src/main/java/eu/opertusmundi/web/config/MvcConfiguration.java
@@ -1,9 +1,35 @@
 package eu.opertusmundi.web.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class MvcConfiguration implements WebMvcConfigurer {
+
+    @Value("${opertusmundi.mvc-async-task-executor.core-pool-size:5}")
+    private int corePoolSize;
+
+    @Value("${opertusmundi.mvc-async-task-executor.max-pool-size:20}")
+    private int maxPoolSize;
+
+    @Bean
+    public ThreadPoolTaskExecutor asyncMvcTaskExecutor() {
+        final ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setThreadNamePrefix("mvc-async-");
+
+        return executor;
+    }
+
+    @Override
+    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+        configurer.setTaskExecutor(asyncMvcTaskExecutor());
+    }
 
 }
